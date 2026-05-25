13d622a548c73af19abb011fb4f378492febdd23
Aamish Husain <aamishhussain7@gmail.com>
2026-05-25 21:33:39 +0530

    Time: 7 ms (78.74%) | Memory: 29.1 MB (28.05%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n=s.size();
+        vector<int>dp(n, 0);
+        if(s[n-1]!='0')return false;
+        dp[0]=1;int reachable=0;
+        for(int i=1;i<n;i++){
+            if(i>=minJump && dp[i-minJump])reachable++;
+            if(i>maxJump && dp[i-maxJump-1])reachable--;
+            if(reachable>0 && s[i]!='1')dp[i]=1;
+        }
+        return dp[n-1];
+    }
+};
